aeb5c33967fbe3d78573b1ceedff92972343f473
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-06-20 19:43:49 +0530

    Add merhcnatsguidecontoller class

--- src/main/java/com/amaap/merchantsguidetogalaxy/controller/MerchantGuideController.java
+++ src/main/java/com/amaap/merchantsguidetogalaxy/controller/MerchantGuideController.java
@@ -1,2 +1,10 @@
-package com.amaap.merchantsguidetogalaxy.controller;public class MerchantGuideController {
+package com.amaap.merchantsguidetogalaxy.controller;
+
+import com.amaap.merchantsguidetogalaxy.controller.dto.Http;
+import com.amaap.merchantsguidetogalaxy.controller.dto.Response;
+
+public class MerchantGuideController {
+    public Response calculateTrade(String path) {
+        return new Response(Http.Ok,"Trade Successful");
+    }
 }
