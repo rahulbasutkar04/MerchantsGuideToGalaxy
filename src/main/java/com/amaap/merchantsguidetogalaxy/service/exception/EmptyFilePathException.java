24ebf0eea6b1a909d3e54d201ae4b17173f2ac8a
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-06-20 20:57:29 +0530

    Add custom exception

---
+++ src/main/java/com/amaap/merchantsguidetogalaxy/service/exception/EmptyFilePathException.java
@@ -0,0 +1,7 @@
+package com.amaap.merchantsguidetogalaxy.service.exception;
+
+public class EmptyFilePathException extends Exception{
+    public EmptyFilePathException(String message) {
+        super(message);
+    }
+}
