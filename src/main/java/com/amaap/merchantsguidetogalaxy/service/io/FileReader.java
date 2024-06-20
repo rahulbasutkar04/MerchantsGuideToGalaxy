85b0e5bdea0ccdc7de3294297716e3f80a0365cd
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-06-20 20:44:55 +0530

    Update FileReader Class

--- src/main/java/com/amaap/merchantsguidetogalaxy/service/io/FileReader.java
+++ src/main/java/com/amaap/merchantsguidetogalaxy/service/io/FileReader.java
@@ -1,2 +1,18 @@
-package com.amaap.merchantsguidetogalaxy.service.io;public class FileReader {
+package com.amaap.merchantsguidetogalaxy.service.io;
+
+import com.amaap.merchantsguidetogalaxy.service.io.exception.NoDataFoundException;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileReader {
+    private List<String> fileData=new ArrayList<>();
+    public List<String> read(String path) throws NoDataFoundException {
+        File file=new File(path);
+
+        if(file.length()==0) throw new NoDataFoundException("No data present in the file");
+
+        return null;
+    }
 }
