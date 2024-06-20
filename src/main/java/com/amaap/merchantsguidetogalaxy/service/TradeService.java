fa6259de3bb125d12bf6cb461aeae68761b51266
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-06-20 20:58:44 +0530

    Update tradeservice class

--- src/main/java/com/amaap/merchantsguidetogalaxy/service/TradeService.java
+++ src/main/java/com/amaap/merchantsguidetogalaxy/service/TradeService.java
@@ -1,2 +1,25 @@
-package com.amaap.merchantsguidetogalaxy.service;public class TradeService {
+package com.amaap.merchantsguidetogalaxy.service;
+
+import com.amaap.merchantsguidetogalaxy.service.exception.EmptyFilePathException;
+import com.amaap.merchantsguidetogalaxy.service.io.FileReader;
+import com.amaap.merchantsguidetogalaxy.service.io.exception.NoDataFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TradeService {
+    private FileReader fileReader;
+
+    public TradeService(FileReader fileReader) {
+        this.fileReader = fileReader;
+    }
+    public boolean calculateTrade(String path) throws NoDataFoundException, EmptyFilePathException {
+
+        if(path.isEmpty()) throw new EmptyFilePathException("File path can not be empty.");
+        List<String> fileData = fileReader.read(path);
+
+
+        return fileData.size() != 0;
+    }
+
 }
