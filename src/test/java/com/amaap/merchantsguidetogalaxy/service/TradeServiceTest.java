cd1b92a99383691101ca69bf36b78ac8e56a9866
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-06-20 20:57:01 +0530

    Update test case for tradeservice

--- src/test/java/com/amaap/merchantsguidetogalaxy/service/TradeServiceTest.java
+++ src/test/java/com/amaap/merchantsguidetogalaxy/service/TradeServiceTest.java
@@ -1,6 +1,8 @@
 package com.amaap.merchantsguidetogalaxy.service;
 
+import com.amaap.merchantsguidetogalaxy.service.exception.EmptyFilePathException;
 import com.amaap.merchantsguidetogalaxy.service.io.FileReader;
+import com.amaap.merchantsguidetogalaxy.service.io.exception.NoDataFoundException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,7 +11,7 @@ class TradeServiceTest {
 
  private FileReader fileReader=new FileReader();
     @Test
-    void shouldBeAbleToReturnTrueIfFileReadingServiceGotTriggered(){
+    void shouldBeAbleToReturnTrueIfFileReadingServiceGotTriggered() throws NoDataFoundException, EmptyFilePathException {
         // arrange
         TradeService tradeService=new TradeService(fileReader);
         String path="D:\\MerchantsGuideToGalaxy\\src\\main\\resources\\Tradedata.txt";
@@ -20,6 +22,19 @@ class TradeServiceTest {
 
         // assert
         assertEquals(expected,actual);
+    }
+
+    @Test
+    void shouldThrowExceptionIFProvidedPathIsEmpty()
+    {
+        // arrange
+        TradeService tradeService=new TradeService(fileReader);
+        String path="";
+
+        // act & assert
+        assertThrows(EmptyFilePathException.class,()->{
+            tradeService.calculateTrade(path);
+        });
     }
 
 }
