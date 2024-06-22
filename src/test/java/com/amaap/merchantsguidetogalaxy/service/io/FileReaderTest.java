6bbc4bd29c6c572a5f16d63bc2963fda591b9fdb
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-06-23 01:38:48 +0530

    Add test case for reading file data

--- src/test/java/com/amaap/merchantsguidetogalaxy/service/io/FileReaderTest.java
+++ src/test/java/com/amaap/merchantsguidetogalaxy/service/io/FileReaderTest.java
@@ -1,9 +1,12 @@
 package com.amaap.merchantsguidetogalaxy.service.io;
 
-import com.amaap.merchantsguidetogalaxy.service.io.exception.NoDataFoundException;
+import com.amaap.merchantsguidetogalaxy.service.io.exception.DataNotFoundException;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.nio.file.FileSystemException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class FileReaderTest {
 
@@ -14,9 +17,26 @@ class FileReaderTest {
         String path = "D:\\MerchantsGuideToGalaxy\\src\\main\\resources\\EmptyData.txt";
 
         // act & assert
-        assertThrows(NoDataFoundException.class, () -> {
+        assertThrows(DataNotFoundException.class, () -> {
             fileReader.read(path);
         });
+
+    }
+
+    @Test
+    void shouldBeAbleToReadTheDataAnExtractAsListOfString() throws DataNotFoundException, FileSystemException {
+        // arrange
+        FileReader fileReader=new FileReader();
+        String path="D:\\MerchantsGuideToGalaxy\\src\\main\\resources\\Tradedata.txt";
+
+        // act
+        List<String> dataInFile =fileReader.read(path);
+
+        System.out.println(dataInFile);
+
+        // assert
+        assertEquals(12,dataInFile.size());
+
 
     }
 
