20ea4fedd6b034159f59468bf25337d7a7db3e86
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-06-23 01:41:05 +0530

    Add code for file data local sotrage

--- src/main/java/com/amaap/merchantsguidetogalaxy/service/TradeService.java
+++ src/main/java/com/amaap/merchantsguidetogalaxy/service/TradeService.java
@@ -2,24 +2,29 @@ package com.amaap.merchantsguidetogalaxy.service;
 
 import com.amaap.merchantsguidetogalaxy.service.exception.EmptyFilePathException;
 import com.amaap.merchantsguidetogalaxy.service.io.FileReader;
-import com.amaap.merchantsguidetogalaxy.service.io.exception.NoDataFoundException;
+import com.amaap.merchantsguidetogalaxy.service.io.exception.DataNotFoundException;
 
+import java.nio.file.FileSystemException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TradeService {
     private FileReader fileReader;
+    private List<String> fileData=new ArrayList<>();
 
     public TradeService(FileReader fileReader) {
         this.fileReader = fileReader;
     }
-    public boolean calculateTrade(String path) throws NoDataFoundException, EmptyFilePathException {
+    public boolean calculateTrade(String path) throws DataNotFoundException, EmptyFilePathException, FileSystemException {
 
         if(path.isEmpty()) throw new EmptyFilePathException("File path can not be empty.");
-        List<String> fileData = fileReader.read(path);
+         fileData = fileReader.read(path);
 
 
         return fileData.size() != 0;
     }
 
+    public List<String> getFileData() {
+        return fileData;
+    }
 }
