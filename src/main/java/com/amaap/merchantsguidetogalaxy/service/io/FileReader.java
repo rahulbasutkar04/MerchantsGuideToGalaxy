2f8041142a467e635e92a0d953e80b03d04084ab
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-06-23 01:39:15 +0530

    Update code to read file data

--- src/main/java/com/amaap/merchantsguidetogalaxy/service/io/FileReader.java
+++ src/main/java/com/amaap/merchantsguidetogalaxy/service/io/FileReader.java
@@ -1,18 +1,37 @@
 package com.amaap.merchantsguidetogalaxy.service.io;
 
-import com.amaap.merchantsguidetogalaxy.service.io.exception.NoDataFoundException;
+import com.amaap.merchantsguidetogalaxy.service.io.exception.DataNotFoundException;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystemException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileReader {
-    private List<String> fileData=new ArrayList<>();
-    public List<String> read(String path) throws NoDataFoundException {
+    public List<String> read(String path) throws DataNotFoundException, FileSystemException {
+         List<String> fileData=new ArrayList<>();
+
         File file=new File(path);
 
-        if(file.length()==0) throw new NoDataFoundException("No data present in the file");
+        if(file.length()==0) throw new DataNotFoundException("No data present in the file");
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(path));
+            for (String line : lines) {
+                String processedLine = line.trim().replaceAll("\\s+", " ");
+                if (!processedLine.isEmpty()) {
+                    fileData.add(processedLine);
+                }
+            }
+        } catch (IOException e) {
+            throw new FileSystemException("Error reading the file: " + e.getMessage());
+        }
 
-        return null;
+        return fileData;
     }
+
+
 }
+
